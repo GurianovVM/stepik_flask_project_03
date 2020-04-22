c13e2acc80a546c2429169f4f78a94755c361968
gvm <79202946014@yandex.ru>
2020-04-22 19:16:39 +0300

    Сделал выборку из БД для главной странице

--- views.py
+++ views.py
@@ -1,11 +1,16 @@
 from flask import render_template
 
-from app import app
-
+from app import app, db
+from models import Category, Dish
 
 @app.route('/')
 def home():
-    return render_template('main.html')
+    category = db.session.query(Category).all()
+    dish_list = []
+    for cat in category:
+        dish = db.session.query(Dish).filter(Dish.category_id == cat.id).limit(3)
+        dish_list.append(dish)
+    return render_template('index.html', category=category, dishes=dish_list)
 
 
 @app.route('/cart/')
